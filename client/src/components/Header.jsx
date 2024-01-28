4d0e5d78aadb5c53c01b654ef7dcaf45fe129060
Aditya Tile <adityatile99@gmail.com>
2024-01-28 23:42:42 +0530

    changes in user model and header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -46,7 +46,7 @@ export default function Header() {
             arrowIcon={false}
             inline
             label={
-              <Avatar alt="user" image={currentUser.profilePicture} rounded />
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
             }
           >
             <Dropdown.Header>
